9a8993b0a78b2d4d5d4a550fde8bc24a46e8e0f5
Stefan Olabina <stefan.olabina@vegait.rs>
2024-05-21 16:38:52 +0200

    Creating VPC network infrastructure with terraform

---
+++ infrastructure/modules/VPC/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr" {
+  type = string
+  default = "172.20.0.0/16"
+}
+
+variable "Owner" {
+    type = string
+    default = "Stefan Olabina"
+}
+
---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "owner" {
+    type = string
+    default = "Stefan Olabina"
+}
+
+variable "region" {
+  description = "AWS region"
+  type = string
+  default = "eu-central-1"
+}
+
+variable "profile" {
+    default = "OlabinaS"
+    type = string
+}
