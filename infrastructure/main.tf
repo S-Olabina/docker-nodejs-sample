13e279fcbaad37f1deff9a7cd0fcb6f4ea39aea5
Stefan Olabina <stefan.olabina@vegait.rs>
2024-05-24 15:44:16 +0200

    Created Elastic Container Registry with terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,3 +19,9 @@ module "vpc" {
 module "iam_assumable_role_with_oidc" {
   source = "./modules/IAM"
 }
+
+module "ecr" {
+  source = "./modules/ECR"
+
+  repository_read_write_access_arns = [module.iam_assumable_role_with_oidc.iam_role_arn]
+}
