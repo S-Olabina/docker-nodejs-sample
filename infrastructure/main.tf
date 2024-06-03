7919193cefbda9a6dc7b94c14d2378244749b0f7
Stefan Olabina <stefan.olabina@vegait.rs>
2024-06-03 09:10:52 +0200

    Helm configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,7 +22,15 @@ provider "aws" {
 }
 
 provider "helm" {
-  # Configuration options
+  kubernetes {
+    host = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_cert)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
 }
 
 module "vpc" {
