76b83f8552bdacbd5e0cb67d32edef66afbf1964
Stefan Olabina <stefan.olabina@vegait.rs>
2024-06-07 11:27:30 +0200

    Changing position of helm-release for psql bitnami and added secret manager

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -78,3 +78,22 @@ module "load_balancer_controller" {
   vpc_id = module.vpc.vpc_id
   region = var.region
 }
+
+module "secrets_manager" {
+  source = "terraform-aws-modules/secrets-manager/aws"
+  version = "~> 1.1.2"
+  name = "postgres-credentials"
+
+  ignore_secret_changes = true
+  secret_string = jsonencode({
+    username = ""
+    password = ""
+    dbname   = ""
+    port     = 0
+  })
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  depends_on = [ module.secrets_manager ]
+  secret_id  = module.secrets_manager.secret_id
+}
